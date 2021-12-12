6ef8b632e73baacc2bce7eccf2a7b4c76b58f203
denyphilip <deny.philip@neosoftmail.com>
2021-12-12 10:39:10 +0530

    message

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Admin;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        Admin::create([
+            'name'=>'Administrator',
+            'email'=>'admin@localhost.com',
+            'role'=>'admin',
+            'password'=>bcrypt('password'),
+        ]);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Http\Controllers\AdminController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+//Route::get('/login',[AdminController::class,'default']);
+Route::group([
+    'prefix'=>config('admin.prefix'),
+    'namespace'=>'App\\Http\\Controllers',
+],function(){
+    Route::get('/login',[AdminController::class,'formLogin'])->name('admin.login');
+    Route::post('login',[AdminController::class,'login']);
+
+    Route::middleware(['auth::admin'])->group(function() {
+        Route::post('logout',[AdminController::class,'logout'])->name('admin.logout');
+        Route::view('/','dashboard')->name('dashboard');
+    });
+});
